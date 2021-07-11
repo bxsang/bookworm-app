29f1f62e2c6a55ab768df688486904e70dfd4b7f
Sang Bui <xsang.bui@gmail.com>
2021-07-11 11:01:16 +0700

    React: implement login

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+// import { connect } from "react-redux";
+import { Provider } from "react-redux";
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
 import NavBar from './components/NavBar';
+import store from "./store";
 
 const App = () => (
-  <BrowserRouter>
-    <NavBar />
-    <Routes />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <NavBar />
+      <Routes />
+    </BrowserRouter>
+  </Provider>
 )
 
 ReactDOM.render(<App />, document.getElementById('app'))
