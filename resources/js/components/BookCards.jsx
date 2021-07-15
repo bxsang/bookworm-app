51ebe0cb7007b37ebf16249ca491dcf8d6e7d362
Sang Bui <xsang.bui@gmail.com>
2021-07-15 20:51:20 +0700

    React: fix book link

--- resources/js/components/BookCards.jsx
+++ resources/js/components/BookCards.jsx
@@ -10,7 +10,7 @@ const BookCards = (props) => {
             <img src={`http://localhost/assets/bookcover/${book.book_cover_photo!==null && book.book_cover_photo.match(/\d+$/)[0]>=1 && book.book_cover_photo.match(/\d+$/)[0]<=10 ? book.book_cover_photo : 'default'}.jpg`} class="img-fluid" alt=""></img>
           </div>
           <div class="thumb-content">
-            <p><Link to={`/book?id=${book.id}`}><b>{`${book.book_title}`}</b></Link><br/><i>{`${book.author.author_name}`}</i></p>
+            <p><Link to={`/book/${book.id}`}><b>{`${book.book_title}`}</b></Link><br/><i>{`${book.author.author_name}`}</i></p>
             <p class="item-price">
               {book.book_price !== book.final_price ? (
                 <strike>{`$${book.book_price}`}</strike>
