0104603b0acf9c4f71c80f7f6620d56c661100f5
Sang Bui <xsang.bui@gmail.com>
2021-07-19 20:34:05 +0700

    React: add footer

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,13 +5,17 @@ import { HashRouter } from 'react-router-dom'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import Routes from './Routes'
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 import store from './store'
 
 const App = () => (
   <Provider store={store}>
     <HashRouter>
       <NavBar />
-      <Routes />
+      <div className="site-content">
+        <Routes />
+      </div>
+      <Footer />
     </HashRouter>
   </Provider>
 )
---
+++ resources/js/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => (
+  <footer className="d-flex justify-content-start">
+    <img
+      alt="Home"
+      src="/assets/bookworm_icon.svg"
+      height="25"
+      className="d-inline-block align-top mx-4 my-4"
+    />
+    <p className="mx-4 my-4">
+      Bookworm
+      <br />
+      Ho Chi Minh City, Vietnam
+      <br />
+      +84.123.456.7890
+    </p>
+  </footer>
+)
+
+export default Footer
