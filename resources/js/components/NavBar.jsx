846fa678bcb2aebfda3aee8417811a68849c9871
Sang Bui <xsang.bui@gmail.com>
2021-07-22 11:07:57 +0700

    React: bold and underline active nav item

--- resources/js/components/NavBar.jsx
+++ resources/js/components/NavBar.jsx
@@ -1,66 +1,94 @@
 /* eslint-disable no-unused-vars */
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
+import { Link, useLocation } from 'react-router-dom'
+import { connect, useDispatch } from 'react-redux'
 import { Nav, Navbar } from 'react-bootstrap'
 
 import { logout } from '../actions/auth'
 
-class NavBar extends Component {
-  constructor(props) {
-    super(props)
-    this.logOut = this.logOut.bind(this)
-  }
+const NavBar = (props) => {
+  const dispatch = useDispatch()
+  const location = useLocation()
 
-  logOut() {
-    this.props.dispatch(logout())
-  }
-
-  render() {
-    return (
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand as={Link} to="/">
-          <img
-            alt="Home"
-            src="/assets/bookworm_icon.svg"
-            height="25"
-            className="d-inline-block align-top"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse>
-          <Nav className="ml-auto">
-            <Nav.Link as={Link} to="/">
-              Home
-            </Nav.Link>
-            <Nav.Link as={Link} to="/shop">
-              Shop
-            </Nav.Link>
-            <Nav.Link as={Link} to="/about">
-              About
-            </Nav.Link>
-            <Nav.Link as={Link} to="/cart">
-              {this.props.cartItems
-                ? `Cart (${Object.keys(this.props.cartItems).length})`
-                : `Cart (0)`}
-            </Nav.Link>
-            {this.props.user ? (
-              <>
-                <Nav.Link as={Link} to="/profile">
-                  Profile
-                </Nav.Link>
-                <Nav.Link onClick={this.logOut}>Logout</Nav.Link>
-              </>
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand as={Link} to="/">
+        <img
+          alt="Home"
+          src="/assets/bookworm_icon.svg"
+          height="25"
+          className="d-inline-block align-top"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse>
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/">
+            {location.pathname === '/' ? (
+              <b>
+                <u>Home</u>
+              </b>
             ) : (
-              <Nav.Link as={Link} to="/login">
-                Login
-              </Nav.Link>
+              `Home`
             )}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    )
-  }
+          </Nav.Link>
+          <Nav.Link as={Link} to="/shop">
+            {location.pathname === '/shop' ? (
+              <b>
+                <u>Home</u>
+              </b>
+            ) : (
+              `Shop`
+            )}
+          </Nav.Link>
+          <Nav.Link as={Link} to="/about">
+            {location.pathname === '/about' ? (
+              <b>
+                <u>About</u>
+              </b>
+            ) : (
+              `About`
+            )}
+          </Nav.Link>
+          <Nav.Link as={Link} to="/cart">
+            {location.pathname === '/cart' ? (
+              <b>
+                <u>
+                  {props.cartItems
+                    ? `Cart (${Object.keys(props.cartItems).length})`
+                    : `Cart (0)`}
+                </u>
+              </b>
+            ) : (
+              <>
+                {props.cartItems
+                  ? `Cart (${Object.keys(props.cartItems).length})`
+                  : `Cart (0)`}
+              </>
+            )}
+          </Nav.Link>
+          {props.user ? (
+            <>
+              <Nav.Link as={Link} to="/profile">
+                {location.pathname === '/profile' ? (
+                  <b>
+                    <u>Profile</u>
+                  </b>
+                ) : (
+                  `Profile`
+                )}
+              </Nav.Link>
+              <Nav.Link onClick={() => dispatch(logout())}>Logout</Nav.Link>
+            </>
+          ) : (
+            <Nav.Link as={Link} to="/login">
+              Login
+            </Nav.Link>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
 }
 
 const mapStateToProps = (state) => {
